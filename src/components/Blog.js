d0f341630ac04b148df5a9a25286beba5bcca0e3
Khaled Mohamed Ibrahim <eng.khaled_ok@outlook.com>
2023-06-12 08:47:59 +0200

    blog section - UI is Done

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import {projects} from '../utils/links'
+import pic from '../assets/images/blogImg.png'
 
 const Blog = () => {
   return (
-    <h2>
-      blog page
-    </h2>
+    <section className="section blog">
+      <div className="section-title">        
+      <h2>blog</h2>
+      <div className="underline"></div>
+      </div>
+
+      <div className="section-center blog-center">
+        {projects.map((i)=>{
+          const {id, img, title, subject}=i
+          return(
+            <div className="card">
+              <div className="card-side card-front">
+                <img src={img} alt="singleItem"/>
+                <div className="card-info">
+                  <h4>{title}</h4>
+                  <p>{subject}</p>
+                </div>
+                <div className="card-footer">
+                  <img src={pic} alt="singlePic"/>
+                  <p>5 min read</p>
+                </div>
+              </div>
+
+              <div className="card-side card-back">
+                <a href="/projects" className='btn'>read more</a>
+              </div>
+            </div>
+          )
+        }).slice(0,3)}
+      </div>
+    </section>
   )
 }
 
