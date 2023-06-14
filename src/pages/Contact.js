18b4630aa39ea2059c99b6c04fb01cb6b1dd4a6a
Khaled Mohamed Ibrahim <eng.khaled_ok@outlook.com>
2023-06-14 13:33:00 +0200

    app is physically ready for deploy

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import emailjs from '@emailjs/browser'
 import {Navbar,Sidebar,Footer} from '../components'
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_loyatsk', 'template_jdnr72g', form.current, 'EfcaVCTSmZFOsd7ZK')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <main>
       <Navbar/>
@@ -10,13 +21,14 @@ const Contact = () => {
           <h2>contact</h2>
           <div className="underline"></div>
           <div className="section-center">
-              <form>
-                <input type="text" className='contact-firstName' placeholder='name'/>
-                <input type="email" className='contact-email' placeholder='email'/>
-                <textarea name="comments" className='contact-textArea' placeholder='Your message' id="" cols="30" rows="5"></textarea>
-                <button type='submit' className='contact-submit'>
-                  send message
-                </button>
+              <form ref={form} onSubmit={sendEmail}>
+                <input type="text" name="from_name" placeholder='name'/>
+                <input type="email" name="from_email" placeholder='email'/>
+                <textarea name="message" placeholder='Your message' id="" cols="30" rows="5"></textarea>
+                {/* <button type='submit' value="Send" >
+                  send
+                </button> */}
+                <input type="submit" value="Send" className='contact-send'/>
               </form>
           </div>
         </div>
