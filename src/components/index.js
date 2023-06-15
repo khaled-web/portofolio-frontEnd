87ba796b5d9fae7d5a01fe5e90d6743b26e6d3f7
Khaled Mohamed Ibrahim <eng.khaled_ok@outlook.com>
2023-06-15 10:02:11 +0200

    The App is physically ready for deploy

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,4 @@
-import Logo from './Logo';
-import FormRow from './FormRow';
-import Alert from './Alert'
 import Navbar from './Navbar';
-import FormRowSelect from './FormRowSelect';
-import Loading from './Loading'
 import Sidebar from './SideBar'
 import Header from './Header'
 import About from './About'
@@ -17,12 +12,7 @@ import Footer from './Footer'
 
 
 export {
- Logo,
- FormRow,
- Alert,
  Navbar,
- FormRowSelect,
- Loading,
  Sidebar,
  Header,
  About,
